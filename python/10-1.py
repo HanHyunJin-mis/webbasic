5e03a0503ab551e048101e3100dc3179f4a1b7e9
HanHyunJin-mis <37561451+HanHyunJin-mis@users.noreply.github.com>
2019-02-21 23:58:51 +0900

    Update 10-1.py

--- python/10-1.py
+++ python/10-1.py
@@ -1,1 +1,28 @@
 from openpyxl import load_workbook as load 
+DIR = '/Users/Administrator/Desktop/webbasic/python/travel.xlsx'
+
+def save_excel(ws):
+    idx = 65
+    for i in range(1, 10):
+        for j in range(1, 10):
+            ws[chr(idx)+ str(j)] = "{} * {} = {}".format(i,j,i*j)
+        idx += 1
+    wb.save(DIR)
+
+def get_excel(ws):
+    idx = 64
+    for i in range(1, 10):
+        for j in range(1, 10):
+            print(ws[chr(idx + j) + str(i)].value, end='')
+        print()
+
+
+wb = load(DIR)
+ws = wb.create_sheet('99단')
+try:
+    save_excel(ws)
+    get_excel(ws)
+except Exception as e:
+    print(e)
+finally:
+    wb.close()
