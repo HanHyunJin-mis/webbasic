3fe401a8afd6dfeb159d19e4f5f771f8c4be52d4
HanHyunJin-mis <37561451+HanHyunJin-mis@users.noreply.github.com>
2019-02-22 23:46:29 +0900

    Add files via upload

---
+++ python/11-1.py
@@ -0,0 +1,28 @@
+# 말뭉치 분석하고 정렬하기
+from random import randint
+
+NOUNS = ['세종', '한글', '한글날']
+
+def get_rand():
+    random_list = []
+    for i in range(11):
+        random_list.append(NOUNS[randint(0,2)])
+    return random_list
+
+def get_rank(random_list):
+    result = {}
+    for n in random_list:
+        if not (n in result):
+            result[n] = 0
+        result[n] +=1
+    result = sorted(result.items(), key = lambda x:x[1], reverse = True)
+    return result
+
+if __name__ == "__main__":
+    result = get_rank(get_rand())
+    for k, v in result:
+        print("{}({})".format(k,v))
+
+# 세종(5)
+# 한글(4)
+# 한글날(2)
